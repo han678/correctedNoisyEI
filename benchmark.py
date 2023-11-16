f2d7e46f5b0717d0b308a8a0a1c03605866d6c57
han678 <56595741+han678@users.noreply.github.com>
2023-11-16 11:16:20 +0100

    Add files via upload

--- benchmark.py
+++ benchmark.py
@@ -11,15 +11,9 @@ from examples.noisy_sythetic_functions import Noisy_synthetic_function, findExtr
 
 parser = argparse.ArgumentParser(description='BO Training for benchmark functions')
 parser.add_argument('--seed', type=int, default=0, metavar='S', help='random seed (default: 1)')
-parser.add_argument('--iter', type=int, default=40, metavar='N',
+parser.add_argument('--iter', type=int, default=5, metavar='N',
                     help='number of iterations to train (default: 100)')
 parser.add_argument('--noise_level', type=float, default=0.05)
-parser.add_argument(
-    "--output_dir", type=str, default=".\\results", help="output directory",
-)
-parser.add_argument(
-    "--acq", type=str, default="NEI", help="acquisition function",
-)
 
 
 def distance_to_optimal(x, optimal):
@@ -30,43 +24,46 @@ if __name__ == '__main__':
     global args
     args = parser.parse_args()
     tkwargs = {"device": torch.device("cuda:0" if torch.cuda.is_available() else "cpu"), "dtype": torch.double}
-    acq = args.acq
+    acqs = ['NEI']# ['PI', 'UCB', "EI_C", 'PI_C', 'EI', "q_NEI", "NEI"]
     # run experiments
+    bench_funcs = []
     noise_level = args.noise_level  # 0.006
-    function = Ackley(dim=5, negate=False))
+    bench_funcs.append(Levy(dim=2, negate=False))
     seed = args.seed
-    noisy_func = Noisy_synthetic_function(function, tkwargs=tkwargs)
-    extreme = findExtrema(noisy_func.function)
-    noisy_func.noise_std = noise_level * (extreme[1] - extreme[0])
-    print(extreme)
-    print("Optimum is:", noisy_func.function._optimal_value)
-    output_dir = args.output_dir
-    os.makedirs(output_dir, exist_ok=True)
+    for function in bench_funcs:
+        noisy_func = Noisy_synthetic_function(function, tkwargs=tkwargs)
+        extreme = findExtrema(noisy_func.function)
+        noisy_func.noise_std = noise_level * (extreme[1] - extreme[0])
+        print(extreme)
+        print("Optimum is:", noisy_func.function._optimal_value)
+        output_dir = f"./benchmark_results/{noisy_func.name}_noise{noise_level}"
+        os.makedirs(output_dir, exist_ok=True)
         while True:
             try:
                 set_determinism(seed + 123456)
-                bo = BO(noisy_func, acq_kind=acq, initial_design="sobol", transform_inputs=True)
-                bo.initialize(int(noisy_func.dim * 3))
-                st = time.time()
-                bo.inference(max_iter=args.iter)
-                print("==> time is {}.".format(time.time() - st))
-                # record the best point
-                acq_dir = os.path.join(output_dir, acq, str(seed))
-                os.makedirs(acq_dir, exist_ok=True)
-                fn_path = os.path.join(acq_dir, f"info.json")
-                all_info = {}
-                # get best objective of the observation set
-                obs_best_f = np.Inf
-                for i in range(len(bo.obs.y)):
-                    obs_x = bo.obs.X[i].tolist()
-                    obs_y = bo.obs.y[i].tolist()
-                    true_f = noisy_func.function.evaluate_true(torch.tensor(obs_x)).tolist()
-                    if true_f <= obs_best_f:
-                        obs_best_f = true_f
-                    all_info.update({f"iteration {i + 1}": {"x": obs_x, "y": obs_y, "best_obj": obs_best_f,
-                                                            "acq_x": bo.best_observed_acq[i]}})
-                with open(fn_path, "w") as f:
-                    json.dump(all_info, f)
+                for acq in acqs:
+                    bo = BO(noisy_func, acq_kind=acq, initial_design="sobol", transform_inputs=True)
+                    bo.initialize(int(noisy_func.dim * 3))
+                    st = time.time()
+                    bo.inference(max_iter=args.iter)
+                    print("==> time is {}.".format(time.time() - st))
+                    # record the best point
+                    acq_dir = os.path.join(output_dir, acq, str(seed))
+                    os.makedirs(acq_dir, exist_ok=True)
+                    fn_path = os.path.join(acq_dir, f"info.json")
+                    all_info = {}
+                    # get best objective of the observation set
+                    obs_best_f = np.Inf
+                    for i in range(len(bo.obs.y)):
+                        obs_x = bo.obs.X[i].tolist()
+                        obs_y = bo.obs.y[i].tolist()
+                        true_f = noisy_func.function.evaluate_true(torch.tensor(obs_x)).tolist()
+                        if true_f <= obs_best_f:
+                            obs_best_f = true_f
+                        all_info.update({f"iteration {i + 1}": {"x": obs_x, "y": obs_y, "best_obj": obs_best_f,
+                                                                "acq_x": bo.best_observed_acq[i]}})
+                    with open(fn_path, "w") as f:
+                        json.dump(all_info, f)
                 break  # If the above succeeds, we break here
             except Exception as e:
                 print(e)
